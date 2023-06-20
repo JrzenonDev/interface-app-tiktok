577d0e9fabfeeb171e32356d767fa10891193829
JrzenonDev <jrobertoonb@gmail.com>
2023-06-20 00:22:35 -0300

    feat: styles statusBar

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "react-native";
 import { Routes } from "./src/routes";
 
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar
+        backgroundColor={"transparent"}
+        barStyle="light-content"
+        translucent={true}
+      />
       <Routes />
     </NavigationContainer>
   );
