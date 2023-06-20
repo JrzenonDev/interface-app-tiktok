d2b0b95e8966dd529ca1c272f40795249f17f7ab
JrzenonDev <jrobertoonb@gmail.com>
2023-06-20 04:39:06 -0300

    feat: create component ButtonNew

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,6 @@
+import { Ionicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { ButtonNew } from "../components/ButtonNew";
 import { Home } from "../pages/Home";
 import { Inbox } from "../pages/Inbox";
 import { New } from "../pages/New";
@@ -12,13 +14,83 @@ export function Routes() {
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#000",
+          borderTopWidth: 0,
+        },
+        tabBarActiveTintColor: "#fff",
       }}
     >
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Search" component={Search} />
-      <Tab.Screen name="New" component={New} />
-      <Tab.Screen name="Inbox" component={Inbox} />
-      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused, size, color }) => {
+            if (focused) {
+              return <Ionicons name="home" size={size} color={color} />;
+            }
+            return <Ionicons name="home-outline" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ focused, size, color }) => {
+            if (focused) {
+              return <Ionicons name="search" size={size} color={color} />;
+            }
+            return <Ionicons name="search-outline" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="New"
+        component={New}
+        options={{
+          tabBarIcon: ({ size }) => {
+            return <ButtonNew size={size} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Inbox"
+        component={Inbox}
+        options={{
+          tabBarIcon: ({ focused, size, color }) => {
+            if (focused) {
+              return (
+                <Ionicons
+                  name="ios-chatbubble-ellipses"
+                  size={size}
+                  color={color}
+                />
+              );
+            }
+            return (
+              <Ionicons
+                name="ios-chatbubble-ellipses-outline"
+                size={size}
+                color={color}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ focused, size, color }) => {
+            if (focused) {
+              return <Ionicons name="person" size={size} color={color} />;
+            }
+            return <Ionicons name="person-outline" size={size} color={color} />;
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 }
